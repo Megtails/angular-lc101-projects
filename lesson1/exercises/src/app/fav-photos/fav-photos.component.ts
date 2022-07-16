a73c560e219aca478c75bab090454d1dd35e24d3
Meghan <stlmeghan@yahoo.com>
2022-07-15 23:23:15 -0500

    all done

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'The Best Television Show Ever: The X-Files';
+  photosTitle = 'The X-Files';
   image3 = 'https://64.media.tumblr.com/a8f10ea438358521ce5fa0ca2c460860/tumblr_inline_p8lkovt2g51taasjf_540.jpg';
   image2 = 'https://www.throwbacks.com/content/images/2018/02/7e7e78cafb18a5bc04c8e9adbae4afd0.jpg';
   image1 = 'https://cdn.vox-cdn.com/thumbor/aL1DzIHIESzz70hNydS6njobhBc=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/3686188/dirt-dave-and-gill.0.jpg';
